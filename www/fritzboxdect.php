e2597402e012b3ae73a98e9555677215b1db279a
geki <h.mth@web.de>
2016-11-20 19:35:02 +0100

    www: add example for AJAX based website polling DECT status

---
+++ www/fritzboxdect.php
@@ -0,0 +1,177 @@
+<?php
+declare(strict_types=1);
+
+namespace FritzBoxDect;
+
+require_once('Phery.php');
+require_once('fritzbox.inc');
+
+use Exception;
+
+class Params
+{
+    # user and password
+    # - user is optional for default setup; simply set any name
+    # - otherwise use FritzBox-User with read/write access
+    static $user = "admin";
+    static $pass = "passw";
+
+    # fritz!box soap server
+    static $base_uri = "https://fritz.box:49443";
+
+    # description of services
+    static $desc = "tr64desc.xml";
+
+    # function signatures, variables and its data types
+    static $scpd = "x_dectSCPD.xml";
+};
+
+function uninit($data, $params, $phery)
+{
+    session_start();
+
+    while($_SESSION['busy'] !== false)
+    {
+        sleep(1);
+    }
+
+    foreach($_SESSION as $key => $val)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    session_write_close();
+
+    return \PheryResponse::factory();
+}
+
+function init($data, $params, $phery)
+{
+    session_start();
+
+    $err = '';
+
+    try
+    {
+        $_SESSION['busy'] = false;
+
+        # receive service description
+        $_SESSION['dect_service'] =
+            \FritzBox\getServiceData(Params::$base_uri, Params::$desc, Params::$scpd);
+
+        if ($_SESSION['dect_service'] === false)
+        {
+            unset($_SESSION['dect_service']);
+
+            throw new Exception("no service found in ". $scpd);
+        }
+    }
+    catch(Exception $e)
+    {
+        $err = nl2br($e->__toString());
+    }
+
+    session_write_close();
+
+    $response = \PheryResponse::factory('#dect_list');
+
+    if ($err != '')
+    {
+        $response->html($err)->show('fast');
+
+        $response->exception($err);
+    }
+    else
+    {
+        $body = 'found service... starting operation...';
+
+        $response->html($body)->show('fast');
+    }
+
+    return $response;
+}
+
+function list_phones($data, $params, $phery)
+{
+    session_start();
+
+    $err = '';
+    $body = '';
+
+    if (isset($_SESSION['dect_service']))
+    {
+        $_SESSION['busy'] = true;
+
+        try
+        {
+            $service = $_SESSION['dect_service'];
+
+            # set user and password
+            $service['login'] = Params::$user;
+            $service['password'] = Params::$pass;
+
+            # create soap client
+            $client = \FritzBox\soapClient($service);
+
+            # function to execute
+            $action = "GetNumberOfDectEntries";
+
+            # execute action published by service
+            $noOfTelephones = (int)\FritzBox\soapAction($client, $action);
+
+            if ($noOfTelephones === false)
+            {
+                throw new Exception("soap fault encountered executing ". $action);
+            }
+
+            $body = "no of dect telephones: ". $noOfTelephones ."<br /><br />";
+
+            $action = "GetGenericDectEntry";
+
+            for($i = 0; $i < $noOfTelephones; $i++)
+            {
+                $result = \FritzBox\soapAction($client, $action,
+                                new \SoapParam($i, 'NewIndex'));
+
+                if ($result === false)
+                {
+                    throw new Exception("soap fault encountered executing ". $action);
+                }
+
+            	$line = ($result['NewActive'] != 0) ? "busy" : "open";
+
+                $body .= "name(". $result['NewName']
+                    .") id(". $result['NewID']
+                    .") line(". $line .")<br />";
+            }
+        }
+        catch(Exception $e)
+        {
+            $err = nl2br($e->__toString());
+        }
+
+        $_SESSION['busy'] = false;
+    }
+    else
+    {
+        $err = 'no dect service available!';
+    }
+
+    session_write_close();
+
+    $response = \PheryResponse::factory('#dect_list');
+
+    if ($err != '')
+    {
+        $response->html($err)->show('fast');
+
+        $response->exception($err);
+    }
+    else
+    {
+        $response->html($body)->show('fast');
+    }
+
+    return $response;
+}
+?>
---
+++ www/index.php
@@ -0,0 +1,123 @@
+<?php
+declare(strict_types=1);
+
+# FIXME
+#
+#   1) trace in exception: remove server path
+#   2) fix exception handling: phery:done should not be executed
+#   3) beforeunload: proper server-side session cleanup
+
+require_once('Phery.php');
+require_once('fritzboxdect.php');
+
+$phery = Phery::instance(array('exceptions' => true));
+
+$phery->set(
+    array(
+        'fritzbox_dect_uninit' => '\FritzBoxDect\uninit',
+        'fritzbox_dect_init' => '\FritzBoxDect\init',
+        'fritzbox_dect_list' => '\FritzBoxDect\list_phones'
+    )
+);
+
+$phery->process();
+?>
+
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN'
+'http://w3c.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+    <title>Fritz!Box DECT</title>
+    <script src="jquery-3.1.1.min.js"></script>
+    <!-- debug -->
+    <!-- <script src="phery-2.7.4.js"></script> -->
+    <script src="phery.min-2.7.4.js"></script>
+    <script type="text/javascript">
+        var dect_list_call = null;
+        var dect_list_timer = null;
+
+        // chrome 55 feature
+        //function sleep(ms)
+        //{
+        //    return new Promise(resolve => setTimeout(resolve, ms));
+        //}
+
+        //async function demo()
+        //{
+        //    console.log('Taking a break...');
+        //    await sleep(10000);
+        //    console.log('Ten seconds later');
+        //}
+
+        // debug
+        //phery.config({'debug.enable': true, 'debug.display.config': true});
+
+        // asnychronous loading of page
+        $(document).ready(
+            function()
+            {
+                // asynchronous ajax call
+                // 3rd param: remove remote call DOM element after use:    {'temp': true}
+                // 4th param: have temporary DOM element to bind events:   false
+                phery.remote('fritzbox_dect_init', null, {'temp': true}, false)
+                    .on({
+                        'phery:done': function()
+                        {
+                            dect_list_call = phery.remote('fritzbox_dect_list', null, null, false)
+                                .on({
+                                    'phery:exception': function(event, exception)
+                                    {
+                                        dect_list_timer.stop();
+
+                                        // FIXME: exception called twice?!
+                                        //alert('dect service polling aborted!');
+                                    }
+                                });
+
+                            dect_list_timer = phery.timer(dect_list_call);
+
+                            dect_list_timer.start(5000);
+                        }
+                    })
+                    .on({
+                        'phery:exception': function(event, exception)
+                        {
+                            dect_list_timer.stop();
+
+                            // FIXME: exception called twice?!
+                            //alert('dect service initialization failed!');
+                        }
+                    })
+                    .phery('remote');
+            }
+        );
+
+        // synchronous unloading of page
+        $(window).on(
+            'beforeunload',
+                function()
+                {
+                    dect_list_timer.stop();
+
+                    dect_list_timer = null;
+                    dect_list_call = null;
+
+                    // FIXME: synchronous ajax call, please? yes, proper server-side cleanup!
+                    //          although this basic example would not need it,
+                    //          a full-featured session will have server-side code, which needs cleanup
+                    //phery.remote('fritzbox_dect_uninit', null, {'temp': true}, true);
+
+                    // chrome 55 feature - wait
+                    //demo();
+
+                    return 'unloaded successfully!';
+                }
+        );
+    </script>
+  </head>
+  <body>
+    <div id='dect_list' name='dect_list'></div>
+  </body>
+</html>
