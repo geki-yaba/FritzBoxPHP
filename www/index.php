33c578847e10db02e67bbeb6f08e70daae35f70c
Hanno Meyer-Thurow <geki-yaba@users.noreply.github.com>
2016-12-14 23:17:57 +0100

    www/index.php: remove last FIXME

--- www/index.php
+++ www/index.php
@@ -1,10 +1,6 @@
 <?php
 declare(strict_types=1);
 
-# FIXME
-#
-#   1) PheryResponse: return success or error versus 'phery:done' and 'phery:exception'
-
 require_once('Phery.php');
 require_once('fritzboxdect.php');
 
